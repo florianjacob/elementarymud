d504f1ff6cf8f05b3db5759a2adb7e8e3c482c5d
Florian Jacob <fjacob@lavabit.com>
2012-08-08 23:54:15 +0200

    Changed classpaths of the server and client scripts to the netbeans autobuild dirs.

---
+++ startclientdaedalus.sh
@@ -0,0 +1,1 @@
+java -cp build/classes/:../marauroa-3.9/dist/Marauroa.jar elementarymud.client.Starter testbot localhost daedalus daedalus test1@test.de
--- startserver.sh
+++ startserver.sh
@@ -1,2 +1,2 @@
-java -cp dist/ElementaryMUD.jar:../marauroa-3.9/dist/Marauroa.jar marauroa.server.marauroad -c server.ini
+java -cp build/classes/:../marauroa-3.9/dist/Marauroa.jar marauroa.server.marauroad -c server.ini
 
